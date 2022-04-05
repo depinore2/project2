fb38b24355f9001c02d72bb34b1d6cbfc99ad94a
Edgar Martinez <leetgardo@gmail.com>
2022-04-05 17:20:02 +0000

    Added an function from fs.

--- class.js
+++ class.js
@@ -1,11 +1,11 @@
-import { readFileSync } from 'fs'
+import { readFileSync, existsSync } from 'fs'
 
 // Tip: you can read a JSON file and convert it into an object using JSON.stringify(). 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify
 
 export class StudentInfoService {
     getStudentGpa(classId, studentId) {
-        // you can read the file like this: readFileSync(`../data/${classId}/${studentId}.json`)
+        // you can read the file like this: readFileSync(`data/${classId}/${studentId}.json`)
     }
     getClassGpa(classId) {
         
