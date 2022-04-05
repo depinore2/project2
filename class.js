a9ccbeef404a56561cbca8ce3d1bac3c11245bfd
Edgar Martinez <leetgardo@gmail.com>
2022-04-05 16:50:06 +0000

    Added a note about json stringify.

--- class.js
+++ class.js
@@ -1,5 +1,8 @@
 import { readFileSync } from 'fs'
 
+// Tip: you can read a JSON file and convert it into an object using JSON.stringify(). 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify
+
 export class StudentInfoService {
     getStudentGpa(classId, studentId) {
         // you can read the file like this: readFileSync(`../data/${classId}/${studentId}.json`)
